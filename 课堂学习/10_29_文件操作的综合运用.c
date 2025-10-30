3510c4efec640dcea7a5812aafdead75af6ff6f6
2504_93454907 <13766397710@139.com>
2025-10-30 16:47:00 +0800

    一次平平无奇的提交

---
+++ 课堂学习/10_29_文件操作的综合运用.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	
+	//需求是什么？嗯，我需要写一个可以储存每天的的自评的程序，并且我输入日期的时候可以查询。 
+	char zhi[60];
+	char ri[60];
+	printf("请你输入你的今日自我评价：")；
+	scanf("%s",zhi);
+	printf("\n请你输入今天的日期：");
+	scanf("%s",ri); 
+	FILE *K;
+	filename=ri+
+	K=fopen(filename,) 
+	return 0； 
+ } 
