b7f9e8aaddd73bd8e5408247f806a6bf33db9ba4
2504_93454907 <13766397710@139.com>
2025-11-02 00:19:24 +0800

    嗯，写了一些书上的练习，感觉又学到了。

---
+++ c primer plus/11_1_ASCII码表.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	/*for(int i=0;i<1000;i++)
+	printf("ASCII码值%d对应的字符为%c\n",i,i);*/
+	
+	char kkk='?';
+	printf("%d,%d",kkk,kkk);
+	return 0;
+} 
---
+++ c primer plus/11_1_第六章的知识运用——for循环的第4种用法.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	for(int a=1;a*a*a<1000;a++)//ÐÞ¸Ä²âÊÔÌõ¼þ¡£ 
+	printf("%d\n",a);
+	return 0;
+ } 
