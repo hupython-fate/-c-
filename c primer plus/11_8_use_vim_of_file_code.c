e60ce09acb101d3fe526fc40aae7899d3ff40da9
hu cheng jian <13766397710@139.com>
2025-11-08 18:54:06 +0800

    美妙至极的解法,关于数组与for循环的结和使用

---
+++ c primer plus/11_8_use_vim_of_file_code.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("this is in today in engilsh write code!\n");
+	printf("why?becese my company have questst!\n");
+	printf("today i learning shell and linux,i`m very heppy.\n");
+	printf("shell is one code linguig ,it very easy,at any rate i think!\n");
+	printf("ok!today exit.cd~");
+    return 0;
+}
---
+++ c primer plus/11_8_第六章后面的编程题6.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("请输入");
+	return 0;
+}
