78588956616d579629618457865eb3f46e01c970
hu cheng jian <13766397710@139.com>
2025-11-21 11:08:05 +0800

    换系统的一次备份和准备,这是为了迎接arch的新开始.加油!

---
+++ 课堂学习/11_19_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+     float a,b,c;
+     printf("shu ru a b c:");
+     scanf("%f %f %f",&a,&b,&c);
+     if(a !=o )
+    {
+        if(b*b-4*a*c>0)
+        {
+            b*b-4*a*c=beita;
+            x1=(-b+sqrt(beita))/2a;
+            x2=(-b-sqrt(beita))/2a;
+            printf("%f %f",x1,x2);
+        }
+        els)
+ 
+
+    }
+     else
+         printf("you no san jiao xing!!");
+
+
+}
