fd4a0acf4ab5bcd237fcb7890cab5f53755f123d
hu cheng jian <13766397710@139.com>
2025-12-07 00:25:32 +0800

    写了c语言项目三字棋，虽然是跟着别人写，但是还是好开心好充实，也许嵌入式的方向并不错，恩，明天接着写三字棋，应该能把它写完

---
+++ c语言的项目/三字棋/12_6_game.h
@@ -0,0 +1,31 @@
+//游戏代码的声明
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#define han 3
+#define lie 3
+
+
+//init qiban of han shu.
+void initqipan(char qiban[han][lie],int a,int b);
+
+//print qiban of han shu 
+void printqipan(char qiban[han][lie],int a,int b);
+
+//玩家下棋
+void wanplay(char qipan[han][lie],int a,int b);
+
+//电脑下棋
+void pcplay(char qipan[han][lie],int a,int b);
+
+//判断输赢的函数
+char pan_duan(char qipan[han][lie],int a,int b);
+//可以约定，如果玩家赢，返回*
+//如果电脑赢，返回#
+//如果平局，返回q
+//如果不是三种清况之一，也就是说，游戏继续，返回c
+//
+
+//判断棋盘格子有没有满的函数。
+char pan_man(char qipan[han][lie],int a,int b);
