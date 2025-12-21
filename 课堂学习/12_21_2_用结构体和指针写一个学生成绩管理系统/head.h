cf689144c3623deb442e07409709b06ce68aad89
hu cheng jian <13766397710@139.com>
2025-12-21 17:25:34 +0800

    完成了学生成绩管理系统的第一个函数，还恶补了一下多文件编程的知识，程序成功运行的那一刻，感觉自己所有的投入都值了，感到非常非常的开心，这真是，太幸福了

---
+++ 课堂学习/12_21_2_用结构体和指针写一个学生成绩管理系统/head.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+#define hunman 50
+#define Subject 9
+
+void input();//函数的声明
+	     //
+struct stu//结构体类型的声明
+{
+	char name[20];
+	char nan_or_nv;//用a表示男性，用b表示女性
+	int age;//年龄。
+	int chengji[9];//用一个数组来储存学生的不同的成绩
+};
+
+extern char name[Subject][30];//声名一个二维数组//用来装科目名的。
+
+extern int a;//a的值为用户输入的科目数。
+//将变量的声明放在头文件中，就会使这个变量变成全局变量！这样，就可以实现变量在不同函数中的复用了。
+extern int b;//b的值为用户输入的学生人数。
+      //
+extern struct stu student_data[hunman];
