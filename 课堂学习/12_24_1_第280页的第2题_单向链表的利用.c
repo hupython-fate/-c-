875fc58559b1c23812b84e559982efec000dbc4b
hu cheng jian <13766397710@139.com>
2025-12-24 12:12:18 +0800

    一次习惯性的提交

---
+++ 课堂学习/12_24_1_第280页的第2题_单向链表的利用.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+struct stu
+{
+	int ID;
+	int math;
+	int english;
+	struct stu *next;
+};
+
+struct stu student_data[5];
+void input()
+{
+	/*
+	int i=0;
+	for(i=0;i<5;i++)
+	{
+		printf("请输入第%d名学生的ID：",i+1);
+		scanf("%d",&student_data[i].ID);
+		printf("请输入第%d名学生的数学成绩：",i+1);
+		scanf("%d",&student_data[]);
+	}*/
+}
+int main(void)
+{
+	
+	return 0;
+}
