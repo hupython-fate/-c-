b17e369ce461212c102a4ca318e0a077b6ed3942
hu cheng jian <13766397710@139.com>
2025-12-28 17:44:29 +0800

    做了一个学生管理系统的框架，具体的功能和使用提验还有待增加和优化，不过，我很开心

--- 课堂学习/12_21_2_用结构体和指针写一个学生成绩管理系统/head.h
+++ 课堂学习/12_21_2_用结构体和指针写一个学生成绩管理系统/head.h
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define hunman 50
 #define Subject 9
 
-void input();//函数的声明
-	     //
 struct stu//结构体类型的声明
 {
 	char name[20];
 	char nan_or_nv;//用a表示男性，用b表示女性
 	int age;//年龄。
 	int chengji[9];//用一个数组来储存学生的不同的成绩
+	struct stu *next;
 };
 
+
 extern char name[Subject][30];//声名一个二维数组//用来装科目名的。
-
 extern int a;//a的值为用户输入的科目数。
-//将变量的声明放在头文件中，就会使这个变量变成全局变量！这样，就可以实现变量在不同函数中的复用了。
 extern int b;//b的值为用户输入的学生人数。
-      //
-extern struct stu student_data[hunman];
+		 
+//第一个文件
+int caret_main();//caret文件的主函数
+void input_we();//函数的声明
+struct stu *caret();//新增界面的主函数，创建一个动态列表。
+void input(struct stu *,int );
+void fprintf_caret(struct stu *);//把用户的输入写入一个文件中
 
-void zhongfen();//计算总分的函数的声名。
--- 课堂学习/12_21_2_用结构体和指针写一个学生成绩管理系统/test.c
+++
@@ -1,8 +0,0 @@
-#include "head.h"
-
-int main(void)
-{
-	input();//获取用户的输入，人数上限为50人，科目上限为9门，
-	
-	return 0;
-}
