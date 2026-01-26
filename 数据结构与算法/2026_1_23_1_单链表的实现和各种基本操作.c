94378c591903934c0efd2d70fbe28477778bcf61
hu cheng jian <13766397710@139.com>
2026-01-26 23:24:59 +0800

    一次提交

--- 数据结构与算法/2026_1_23_1_单链表的实现和各种基本操作.c
+++ 数据结构与算法/2026_1_23_1_单链表的实现和各种基本操作.c
@@ -37,12 +37,60 @@ int  xiao_hui(linklist p)
 		linklist qq=p;
 		p=p->next;
 		free(qq);
+		qq=NULL;
 	}
 	return OK;
 }
 
 //第三个补充算法，清空单链表
 //
+int qing_kong(linklist p)
+{
+	linklist k=p->next;
+	while(k!=NULL)
+	{
+		linklist q=k->next;//q指向要消毁的结点的下一个结点的地址
+		free(k);
+		k=q;
+	}
+	p->next=NULL;
+	return OK;
+}
+
+//第四个补充的算法
+//求链表的表长.
+int qiu_biao_chang(linklist p)
+{
+	//因为这个链表是带头结点的,而表长就是有多少个数据元素,所以,不能把头结点计入其中.
+	//用for循环吧,有天然的计数器.
+	int i;
+	p=p->next;
+	for(i=0;p!=NULL;i++)
+		p=p->next;
+	return i;
+}
+
+
+//以下是很重要的算法,是链表的高级操作的基础.
+//
+//1,取值,取单链表中第i个元素的值.
+//
+people qu_zhi(linklist p,int x)//返回类型应为自己规定的那一种.
+{
+	int i=0;
+	linklist q=p;
+	for(i=0;i<x && q!=NULL;i++)
+	{
+		q=q->next;
+	}
+	people k=q->date;
+	return k;
+}
+//2,查找,给定某个数据,返回这个数据的地址或者序号.
+//也就是,按值查找.
+//
+
+
 int main(void)
 {
 	linklist l;//定义一个指针，指向头结点的指针。
