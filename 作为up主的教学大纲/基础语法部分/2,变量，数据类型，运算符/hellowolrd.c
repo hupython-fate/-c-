f02cac313d6a255fe09425bdd21dd064d4a1d3d9
hu cheng jian <13766397710@139.com>
2026-02-03 22:39:18 +0800

    一次提交

---
+++ 作为up主的教学大纲/基础语法部分/2,变量，数据类型，运算符/hellowolrd.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello world!");//输出函数
+    //print:打印
+    //f：farmet格式化的缩写
+    //printf:格式化打印。
+    return 0;
+}
---
+++ 作为up主的教学大纲/基础语法部分/2,变量，数据类型，运算符/输出爱心.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("******    *****\n");
+    printf(" ************\n");
+    printf("   *********\n");
+    printf("     ******\n");
+    printf("       ***\n");
+    printf("        *\n");
+
+    //课后作业：输出“我是最帅的！”；
+    
+    return 0;
+}
